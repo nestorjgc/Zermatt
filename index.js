83a39f64ad722c999103b1e9aa8f2079c0c5ed21
Nestor Garcia <nestorjgcc@hotmail.com>
2022-10-28 17:45:20 -0700

    first commit

---
+++ index.js
@@ -0,0 +1,9 @@
+function sunset() {
+  document.getElementById("p2").style.filter = "saturate(3)";
+  document.getElementById("navbar-black").style.backgroundColor = "black";
+}
+
+function cloudy() {
+  document.getElementById("p2").style.filter = "none";
+  document.getElementById("navbar-black").style.backgroundColor = "#477890";
+}
