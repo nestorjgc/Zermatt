14cced884ff9c7beb48abf08c8e96b98e93fd9a6
Nestor Garcia <nestorjgcc@hotmail.com>
2022-11-20 19:55:09 -0800

    ready

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 function sunset() {
   document.getElementById("p2").style.filter = "saturate(3)";
   document.getElementById("navbar-black").style.backgroundColor = "black";
+  document.getElementById("p2").style.transition = "all 1s";
 }
 
 function cloudy() {
